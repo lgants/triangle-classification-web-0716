aff21ee20c4b40a217dd7e4b69a28b7a990c3b9d
lgants <lgants@gmail.com>
2016-08-04 16:57:55 -0400

    triangle classification lab complete -ltg

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,20 @@
 class Triangle
-  # write code here
+  attr_accessor :length_one, :length_two, :length_three
+
+  def initialize(length_one,length_two,length_three)
+    @length_one = length_one
+    @length_two = length_two
+    @length_three = length_three
+  end
+
+  def kind
+    sides = [length_one,length_two,length_three].sort
+    raise TriangleError if sides.first <= 0 || sides[2] >= sides[1] + sides[0]
+    return :equilateral if sides.uniq.length  == 1
+    return :isosceles if sides.uniq.length  == 2
+    :scalene
+  end
+end
+
+class TriangleError < StandardError
 end
